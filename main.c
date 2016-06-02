f777fb0bcb4f926b1e70e5f737b4d75ae176dc2f
Pecli <Pecli@Pecli-PC>
2016-06-02 21:12:20 +0200

    création projet

--- main.c
+++ main.c
@@ -1,65 +1,57 @@
 #include <xc.h>
-#include <stdio.h>
+#include "maitre.h"
+#include "i2c.h"
+#include "test.h"
 
 /**
- * Bits de configuration:
+ * Bits de configuration: ! à contrôler !
  */
-#pragma config FOSC = INTIO67  // Osc. interne, A6 et A7 comme IO.
-#pragma config IESO = OFF      // Pas d'osc. au démarrage.
-#pragma config FCMEN = OFF     // Pas de monitorage de l'oscillateur.
+#pragma config FOSC = INTIO67   // Osc. interne, A6 et A7 comme IO.
+#pragma config IESO = OFF       // Pas d'osc. au démarrage.
+#pragma config FCMEN = OFF      // Pas de monitorage de l'oscillateur.
 
 // Nécessaires pour ICSP / ICD:
-#pragma config MCLRE = EXTMCLR // RE3 est actif comme master reset.
-#pragma config WDTEN = OFF     // Watchdog inactif.
-#pragma config LVP = OFF       // Single Supply Enable bits off.
+#pragma config MCLRE = EXTMCLR  // RE3 est actif comme master reset.
+#pragma config WDTEN = OFF      // Watchdog inactif.
+#pragma config LVP = OFF        // Single Supply Enable bits off.
+
+// Configuration de l'entrée RC -> PWM:
+
+// A compléter
+
+// Configuration de l'entrée bluetooth -> UART
+
+// A compléter
+
+// Configuration 
+
+#ifndef TEST
 
 /**
- * Fonction qui transmet un caractère à la EUSART.
- * Il s'agit de l'implémentation d'une fonction système qui est
- * appelée par <code>printf</code>.
- * Cette implémentation envoie le caractère à la UART. Si un terminal
- * est connecté aux sorties RX / TX, il affichera du texte.
- * @param data Le code ASCII du caractère à afficher.
-*/
-void putch(char data) {
-    while( ! TX1IF);
-    TXREG1 = data;
-}
-
-/**
- * Initialise la sortie 1 de la EUSART.
- * Si le UP est à 1MHz, le Virtual Terminal dans Proteus
- * doit être configuré comme suit:
- * - 1200 bauds.
- * - Transmission 8 bits.
- * - Bit de stop activé.
+ * Point d'entrée des interruptions basse priorité.
  */
-void initialiseEUSART() {
-    // Pour une fréquence de 1MHz, ceci donne 1200 bauds:
-    SPBRG = 12;
-    SPBRGH = 0;
-    // Configure RC6 et RC7 comme entrées digitales, pour que
-    // la EUSART puisse en prendre le contrôle:
-    TRISCbits.RC6 = 1;
-    TRISCbits.RC7 = 1;
+void low_priority interrupt interruptionsBassePriorite() {
     
-    // Configure la EUSART:
-    // (BRGH et BRG16 sont à leur valeurs par défaut)
-    // (TX9 est à sa valeur par défaut)
-    RCSTAbits.SPEN = 1;  // Active la EUSART.
-    TXSTAbits.SYNC = 0;  // Mode asynchrone.
-    TXSTAbits.TXEN = 1;  // Active l'émetteur.
+        maitreInterruptions();  
 }
 
 /**
- * Point d'entrée du programme.
+ * Point d'entrée.
+ * Lancement du programme en récepteur.
  */
 void main(void) {
-    unsigned char n;
+  
+    maitreMain();
     
-    initialiseEUSART();
-
-    for (n = 0; n < 100; n++) {
-        printf("v1 Hello world: %d\r\n", n);
-    }
+    while(1);
 }
+#endif
+
+#ifdef TEST
+void main() {
+    initialiseTests();
+    testI2c();
+    finaliseTests();
+    while(1);
+}
+#endif
